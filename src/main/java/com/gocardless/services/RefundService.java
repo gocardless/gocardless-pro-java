0969c9d019a8470c38fc5d2c38539ec958ce1f07
Crankshaft Robot <robot+crankshaft@gocardless.com>
2015-05-15 10:51:41 +0000

    Crankshaft Build #BU000000WRV3V4 @ 2015-05-15 10:51:41 +0000

--- src/main/java/com/gocardless/services/RefundService.java
+++ src/main/java/com/gocardless/services/RefundService.java
@@ -43,10 +43,10 @@ public class RefundService {
      * Creates a new refund object.
      * 
      * This fails with:<a name="refund_payment_invalid_state"></a><a
-     * name="total_amount_confirmation_invalid"></a><a name="total_number_of_refunds_exceeded"></a>
+     * name="total_amount_confirmation_invalid"></a><a name="number_of_refunds_exceeded"></a>
      * 
-     *
-     * - `refund_payment_invalid_state` error if the linked
+     * -
+     * `refund_payment_invalid_state` error if the linked
      * [payment](https://developer.gocardless.com/pro/2015-04-29/#api-endpoints-payments) isn't either
      * `confirmed` or `paid_out`.
      * 
@@ -55,7 +55,7 @@ public class RefundService {
      * processes from creating refunds without awareness of each other.
      * 
      * -
-     * `total_number_of_refunds_exceeded` if five or more refunds have already been created against the
+     * `number_of_refunds_exceeded` if five or more refunds have already been created against the
      * payment.
      * 
      */
@@ -96,10 +96,10 @@ public class RefundService {
      * Creates a new refund object.
      * 
      * This fails with:<a name="refund_payment_invalid_state"></a><a
-     * name="total_amount_confirmation_invalid"></a><a name="total_number_of_refunds_exceeded"></a>
+     * name="total_amount_confirmation_invalid"></a><a name="number_of_refunds_exceeded"></a>
      * 
-     *
-     * - `refund_payment_invalid_state` error if the linked
+     * -
+     * `refund_payment_invalid_state` error if the linked
      * [payment](https://developer.gocardless.com/pro/2015-04-29/#api-endpoints-payments) isn't either
      * `confirmed` or `paid_out`.
      * 
@@ -108,7 +108,7 @@ public class RefundService {
      * processes from creating refunds without awareness of each other.
      * 
      * -
-     * `total_number_of_refunds_exceeded` if five or more refunds have already been created against the
+     * `number_of_refunds_exceeded` if five or more refunds have already been created against the
      * payment.
      * 
      */
