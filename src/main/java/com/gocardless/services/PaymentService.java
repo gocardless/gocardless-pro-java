3c31e89ed540925ac27b7bdedfeaf5afe246ddfc
Crankshaft Robot <robot+crankshaft@gocardless.com>
2015-10-20 11:05:40 +0000

    Crankshaft[BU0000025455J6] Client - Java @ 2015-10-20 11:05:40 +0000

--- src/main/java/com/gocardless/services/PaymentService.java
+++ src/main/java/com/gocardless/services/PaymentService.java
@@ -202,7 +202,7 @@ public class PaymentService {
         /**
          * An optional payment reference. This will be appended to the mandate reference on your customer's
          * bank statement. For Bacs payments this can be up to 10 characters, for SEPA payments the limit is
-         * 140 characters, and for Autogiro payments the limit is 16 characters.
+         * 140 characters, and for Autogiro payments the limit is 11 characters.
          */
         public PaymentCreateRequest withReference(String reference) {
             this.reference = reference;
