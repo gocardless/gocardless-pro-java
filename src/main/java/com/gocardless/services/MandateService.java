054886b826adee375efef738280128ede3acc862
Robot <robot@gocardless.com>
2018-10-17 12:26:28 +0000

    Changes generated by 945831ae367a2902e5b2ca61f193b600f757b6aa

--- src/main/java/com/gocardless/services/MandateService.java
+++ src/main/java/com/gocardless/services/MandateService.java
@@ -165,7 +165,6 @@ public class MandateService {
         /**
          * <a name="mandates_scheme"></a>Direct Debit scheme to which this mandate and associated payments
          * are submitted. Can be supplied or automatically detected from the customer's bank account.
-         * Currently "autogiro", "bacs", "becs", "betalingsservice", and "sepa_core" are supported.
          */
         public MandateCreateRequest withScheme(String scheme) {
             this.scheme = scheme;
