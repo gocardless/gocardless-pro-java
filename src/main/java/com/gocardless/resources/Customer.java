2ae842086595a644a3e6194008cca359adefc8ff
Robot <robot@gocardless.com>
2018-11-21 14:32:37 +0000

    Changes generated by e0ba0d6f6d4ca6a0918698969809db020c454342

--- src/main/java/com/gocardless/resources/Customer.java
+++ src/main/java/com/gocardless/resources/Customer.java
@@ -70,7 +70,10 @@ public class Customer {
     }
 
     /**
-     * Customer's company name. Required unless a `given_name` and `family_name` are provided.
+     * Customer's company name. Required unless a `given_name` and `family_name` are provided. For
+     * Canadian customers, the use of a `company_name` value will mean that any mandate created from this
+     * customer will be considered to be a "Business PAD" (otherwise, any mandate will be considered to
+     * be a "Personal PAD").
      */
     public String getCompanyName() {
         return companyName;
