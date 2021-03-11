6d4d6bf4b258ade68f7570337700e251e859dc77
Robot <robot@gocardless.com>
2021-03-11 10:58:48 +0000

    Changes generated by 18cad2cbb7ff39e679ce8094005ecedb7e25bf7c

--- src/main/java/com/gocardless/services/SubscriptionService.java
+++ src/main/java/com/gocardless/services/SubscriptionService.java
@@ -143,8 +143,9 @@ public class SubscriptionService {
      * No payments will be created until it is resumed.
      * 
      * This can only be used when a subscription collecting a fixed number of payments (created using
-     * `count`)
-     * or when they continue forever (created without `count` or `end_date`)
+     * `count`),
+     * when they continue forever (created without `count` or `end_date`) or
+     * the subscription is paused for a number of cycles.
      * 
      * When `pause_cycles` is omitted the subscription is paused until the [resume
      * endpoint](#subscriptions-resume-a-subscription) is called.
@@ -165,7 +166,10 @@ public class SubscriptionService {
      * 
      * - `validation_failed` if invalid data is provided when attempting to pause a subscription.
      * 
-     * - `subscription_not_active` if the subscription is no longer active.
+     * - `subscription_paused_cannot_update_cycles` if the subscription is already paused for a number of
+     * cycles and the request provides a value for `pause_cycle`.
+     * 
+     * - `subscription_cannot_be_paused` if the subscription cannot be paused.
      * 
      * - `subscription_already_ended` if the subscription has taken all payments.
      * 
@@ -896,8 +900,9 @@ public class SubscriptionService {
      * No payments will be created until it is resumed.
      * 
      * This can only be used when a subscription collecting a fixed number of payments (created using
-     * `count`)
-     * or when they continue forever (created without `count` or `end_date`)
+     * `count`),
+     * when they continue forever (created without `count` or `end_date`) or
+     * the subscription is paused for a number of cycles.
      * 
      * When `pause_cycles` is omitted the subscription is paused until the [resume
      * endpoint](#subscriptions-resume-a-subscription) is called.
@@ -918,7 +923,10 @@ public class SubscriptionService {
      * 
      * - `validation_failed` if invalid data is provided when attempting to pause a subscription.
      * 
-     * - `subscription_not_active` if the subscription is no longer active.
+     * - `subscription_paused_cannot_update_cycles` if the subscription is already paused for a number of
+     * cycles and the request provides a value for `pause_cycle`.
+     * 
+     * - `subscription_cannot_be_paused` if the subscription cannot be paused.
      * 
      * - `subscription_already_ended` if the subscription has taken all payments.
      * 
