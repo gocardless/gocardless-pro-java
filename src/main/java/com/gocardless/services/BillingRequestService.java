13821224abe7517b48df70213ad567b3ab9e2f75
Robot <robot@gocardless.com>
2023-02-28 14:00:37 +0000

    Changes generated by 933542358fd2bbe208ee4f622d2b88cbb4b53737

--- src/main/java/com/gocardless/services/BillingRequestService.java
+++ src/main/java/com/gocardless/services/BillingRequestService.java
@@ -2,11 +2,9 @@ package com.gocardless.services;
 
 import com.gocardless.http.*;
 import com.gocardless.resources.BillingRequest;
-import com.google.common.base.Joiner;
 import com.google.common.collect.ImmutableMap;
 import com.google.gson.annotations.SerializedName;
 import com.google.gson.reflect.TypeToken;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -139,15 +137,6 @@ public class BillingRequestService {
      */
     public BillingRequestChooseCurrencyRequest chooseCurrency(String identity) {
         return new BillingRequestChooseCurrencyRequest(httpClient, identity);
-    }
-
-    /**
-     * Returns all institutions valid for a Billing Request.
-     * 
-     * This endpoint is currently supported only for FasterPayments.
-     */
-    public BillingRequestListInstitutionsRequest listInstitutions(String identity) {
-        return new BillingRequestListInstitutionsRequest(httpClient, identity);
     }
 
     /**
@@ -2197,96 +2186,6 @@ public class BillingRequestService {
         @Override
         protected String getRequestEnvelope() {
             return "data";
-        }
-    }
-
-    /**
-     * Request class for {@link BillingRequestService#listInstitutions }.
-     *
-     * Returns all institutions valid for a Billing Request.
-     * 
-     * This endpoint is currently supported only for FasterPayments.
-     */
-    public static final class BillingRequestListInstitutionsRequest
-            extends GetRequest<BillingRequest> {
-        @PathParam
-        private final String identity;
-        private List<String> ids;
-        private String search;
-
-        /**
-         * ID(s) of the institution(s) to retrieve. More than one ID can be specified using a
-         * comma-separated string.
-         */
-        public BillingRequestListInstitutionsRequest withIds(List<String> ids) {
-            this.ids = ids;
-            return this;
-        }
-
-        /**
-         * ID(s) of the institution(s) to retrieve. More than one ID can be specified using a
-         * comma-separated string.
-         */
-        public BillingRequestListInstitutionsRequest withIds(String ids) {
-            if (this.ids == null) {
-                this.ids = new ArrayList<>();
-            }
-            this.ids.add(ids);
-            return this;
-        }
-
-        /**
-         * A search substring for retrieving institution(s), based on the institution's name.
-         */
-        public BillingRequestListInstitutionsRequest withSearch(String search) {
-            this.search = search;
-            return this;
-        }
-
-        private BillingRequestListInstitutionsRequest(HttpClient httpClient, String identity) {
-            super(httpClient);
-            this.identity = identity;
-        }
-
-        public BillingRequestListInstitutionsRequest withHeader(String headerName,
-                String headerValue) {
-            this.addHeader(headerName, headerValue);
-            return this;
-        }
-
-        @Override
-        protected Map<String, String> getPathParams() {
-            ImmutableMap.Builder<String, String> params = ImmutableMap.builder();
-            params.put("identity", identity);
-            return params.build();
-        }
-
-        @Override
-        protected Map<String, Object> getQueryParams() {
-            ImmutableMap.Builder<String, Object> params = ImmutableMap.builder();
-            params.putAll(super.getQueryParams());
-            if (ids != null) {
-                params.put("ids", Joiner.on(",").join(ids));
-            }
-            if (search != null) {
-                params.put("search", search);
-            }
-            return params.build();
-        }
-
-        @Override
-        protected String getPathTemplate() {
-            return "billing_requests/:identity/list_institutions";
-        }
-
-        @Override
-        protected String getEnvelope() {
-            return "billing_requests";
-        }
-
-        @Override
-        protected Class<BillingRequest> getResponseClass() {
-            return BillingRequest.class;
         }
     }
 }
--- src/main/java/com/gocardless/services/InstitutionService.java
+++ src/main/java/com/gocardless/services/InstitutionService.java
@@ -2,8 +2,10 @@ package com.gocardless.services;
 
 import com.gocardless.http.*;
 import com.gocardless.resources.Institution;
+import com.google.common.base.Joiner;
 import com.google.common.collect.ImmutableMap;
 import com.google.gson.reflect.TypeToken;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -37,6 +39,17 @@ public class InstitutionService {
     public InstitutionListRequest<Iterable<Institution>> all() {
         return new InstitutionListRequest<>(httpClient,
                 ListRequest.<Institution>iteratingExecutor());
+    }
+
+    /**
+     * Returns all institutions valid for a Billing Request.
+     * 
+     * This endpoint is currently supported only for FasterPayments.
+     */
+    public InstitutionListForBillingRequestRequest<Iterable<Institution>> listForBillingRequest(
+            String identity) {
+        return new InstitutionListForBillingRequestRequest<>(httpClient,
+                ListRequest.<Institution>iteratingExecutor(), identity);
     }
 
     /**
@@ -80,6 +93,97 @@ public class InstitutionService {
         @Override
         protected String getPathTemplate() {
             return "institutions";
+        }
+
+        @Override
+        protected String getEnvelope() {
+            return "institutions";
+        }
+
+        @Override
+        protected TypeToken<List<Institution>> getTypeToken() {
+            return new TypeToken<List<Institution>>() {};
+        }
+    }
+
+    /**
+     * Request class for {@link InstitutionService#listForBillingRequest }.
+     *
+     * Returns all institutions valid for a Billing Request.
+     * 
+     * This endpoint is currently supported only for FasterPayments.
+     */
+    public static final class InstitutionListForBillingRequestRequest<S>
+            extends ListRequest<S, Institution> {
+        @PathParam
+        private final String identity;
+        private List<String> ids;
+        private String search;
+
+        /**
+         * ID(s) of the institution(s) to retrieve. More than one ID can be specified using a
+         * comma-separated string.
+         */
+        public InstitutionListForBillingRequestRequest<S> withIds(List<String> ids) {
+            this.ids = ids;
+            return this;
+        }
+
+        /**
+         * ID(s) of the institution(s) to retrieve. More than one ID can be specified using a
+         * comma-separated string.
+         */
+        public InstitutionListForBillingRequestRequest<S> withIds(String ids) {
+            if (this.ids == null) {
+                this.ids = new ArrayList<>();
+            }
+            this.ids.add(ids);
+            return this;
+        }
+
+        /**
+         * A search substring for retrieving institution(s), based on the institution's name.
+         */
+        public InstitutionListForBillingRequestRequest<S> withSearch(String search) {
+            this.search = search;
+            return this;
+        }
+
+        private InstitutionListForBillingRequestRequest(HttpClient httpClient,
+                ListRequestExecutor<S, Institution> executor, String identity) {
+            super(httpClient, executor, "GET");
+            this.identity = identity;
+        }
+
+        public InstitutionListForBillingRequestRequest<S> withHeader(String headerName,
+                String headerValue) {
+            this.addHeader(headerName, headerValue);
+            return this;
+        }
+
+        @Override
+        protected Map<String, String> getPathParams() {
+            ImmutableMap.Builder<String, String> params = ImmutableMap.builder();
+            params.put("identity", identity);
+            return params.build();
+        }
+
+        @Override
+        protected Map<String, Object> getQueryParams() {
+            ImmutableMap.Builder<String, Object> params = ImmutableMap.builder();
+            params.putAll(super.getQueryParams());
+            if (ids != null) {
+                params.put("ids", Joiner.on(",").join(ids));
+            }
+            if (search != null) {
+                params.put("search", search);
+            }
+            return params.build();
+        }
+
+        @Override
+        protected String getPathTemplate() {
+            return "billing_requests/:identity/institutions";
         }
 
         @Override
